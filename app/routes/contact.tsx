2479856182c5d849ac7690a286f22080d22c8c10
naimroslan <heyitsnaim@gmail.com>
2024-05-09 17:50:19 +0800

    feat(contact): added back to home button

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,9 +1,9 @@
-import { useEffect } from "react";
+import { useNavigate } from "@remix-run/react";
+import { IoIosArrowRoundBack } from "react-icons/io";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 
-import naim from "~/assets/naimroslan.png"
-
 export default function Contact() {
+  const navigate = useNavigate()
 
   const handleLinkedInOnClick = () => {
     window.open('https://www.linkedin.com/in/muhammad-naim-bin-roslan/', '_blank');
@@ -14,27 +14,32 @@ export default function Contact() {
   }
 
   return(
-    <div className="font-SpaceGrotesk">
-      <div className="flex h-screen justify-center items-center">
-        <div className="flex flex-col space-y-8">
-          <div className="flex flex-row items-center space-x-4">
-            <div className="flex flex-col font-medium text-7xl">
-              <div>REACH</div>
-              <div>ME</div>
-              <div>OUT</div>
+    <div className="h-screen overflow-hidden">
+      <div className="font-SpaceGrotesk h-full flex flex-col">
+        <div className="p-4 cursor-pointer" onClick={() =>  navigate("/")}>
+          <IoIosArrowRoundBack size={30} />
+        </div>
+        <div className="flex justify-center items-center mt-14">
+          <div className="flex flex-col space-y-8">
+            <div className="flex flex-row items-center space-x-4">
+              <div className="flex flex-col font-medium text-7xl">
+                <div>REACH</div>
+                <div>ME</div>
+                <div>OUT</div>
+              </div>
+              <div>
+                
+              </div>
             </div>
-            <div>
-              
-            </div>
-          </div>
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-row items-center space-x-2 cursor-pointer" onClick={handleLinkedInOnClick}>
-              <SiLinkedin />
-              <div>LinkedIn</div>
-            </div>
-            <div className="flex flex-row items-center space-x-2 cursor-pointer" onClick={handleGithubOnClick}>
-              <SiGithub />
-              <div>Github</div>
+            <div className="flex flex-row justify-between">
+              <div className="flex flex-row items-center space-x-2 cursor-pointer" onClick={handleLinkedInOnClick}>
+                <SiLinkedin />
+                <div>LinkedIn</div>
+              </div>
+              <div className="flex flex-row items-center space-x-2 cursor-pointer" onClick={handleGithubOnClick}>
+                <SiGithub />
+                <div>Github</div>
+              </div>
             </div>
           </div>
         </div>
